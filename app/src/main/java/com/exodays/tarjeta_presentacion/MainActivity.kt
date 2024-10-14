bfc1d1bc21868933e2d1bd72da690ab2459a2265
Exodays <77130050+Jery-salinas@users.noreply.github.com>
2024-10-13 23:08:46 -0600

    New Up

--- app/src/main/java/com/exodays/tarjeta_presentacion/MainActivity.kt
+++ app/src/main/java/com/exodays/tarjeta_presentacion/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.exodays.tarjeta_presentacion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        updateUI()
+
+    }
+
+    private fun updateUI(
+        name: String = "Exodays inc",
+        correo: String = "jerrsalinas5@gmail.com",
+        telefono: String = "+52 294 181 5097",
+        sitio: String = "www.exo.mx"
+    ) {
+        // Establece los valores de texto en las vistas
+        binding.txtname.text = name
+        binding.txtemail.text = correo
+        binding.txtphone.text = telefono
+        binding.txtwebsite.text = sitio
     }
 }
