8f1227d40e9de13d2b3fcaae49f1d4a82caa8659
Exodays <77130050+Jery-salinas@users.noreply.github.com>
2024-10-15 22:58:37 -0600

    Agregando menu de opciones

--- app/src/main/java/com/exodays/tarjeta_presentacion/MainActivity.kt
+++ app/src/main/java/com/exodays/tarjeta_presentacion/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.exodays.tarjeta_presentacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +11,10 @@ import androidx.core.view.WindowInsetsCompat
 import com.exodays.tarjeta_presentacion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,21 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         updateUI()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_edit){
+            val intent = Intent(this,EditActivity::class.java)
+
+        }
 
     }
 
